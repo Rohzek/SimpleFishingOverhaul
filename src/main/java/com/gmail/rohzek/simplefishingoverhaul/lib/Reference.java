a634680809eabe9917a0b9508e112e4061e29c1b
Rohzek <rohzek@gmail.com>
2023-02-20 22:49:07 -0600

    Initial Commit

---
+++ src/main/java/com/gmail/rohzek/simplefishingoverhaul/lib/Reference.java
@@ -0,0 +1,10 @@
+package com.gmail.rohzek.simplefishingoverhaul.lib;
+
+public class Reference 
+{
+	// Mod ID
+	public static final String MODID = "simplefishingoverhaul";
+	
+	// Just like MODID but with : attached to save time while assigning resource locations
+	public static final String RESOURCEID = MODID + ":";
+}
---
+++ src/main/java/com/gmail/rohzek/simplefishingoverhaul/tools/SimpleFishingRod.java
@@ -0,0 +1,11 @@
+package com.gmail.rohzek.simplefishingoverhaul.tools;
+
+import net.minecraft.world.item.FishingRodItem;
+
+public class SimpleFishingRod extends FishingRodItem
+{
+	public SimpleFishingRod(Properties props) 
+	{
+		super(props);
+	}
+}
